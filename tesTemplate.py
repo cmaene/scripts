dabeb88d64ef0c80ddf8a077b69173c95336ad01
cmaene <cmaene@gmail.com>
2015-06-23 14:26:49 -0500

    first commit - output from pyTemplate.sh

---
+++ tesTemplate.py
@@ -0,0 +1,21 @@
+#/usr/bin/python
+
+''' VERSION : Python 2.7.6
+AUTHOR      : chieko
+DATE        : Tue Jun 23 14:26:05 CDT 2015
+DIRECTORY   : /home/chieko/scripts
+SHORT DESC. : my python script
+'''
+
+# list modules
+import
+
+# runs all the functions
+def main():
+    # do something or run function(s) here
+
+# =============================
+#This idiom means the below code only runs when executed from command line
+if __name__ == '__main__':
+    main()
+
