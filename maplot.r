585360e1c8c6fad30ccb1b9381598dc589c1e14f
cmaene <cmaene@gmail.com>
2015-06-25 15:19:18 -0500

    add maplot.r - add description

--- maplot.r
+++ maplot.r
@@ -1,12 +1,13 @@
 #!/usr/bin/Rscript
 
-# e.g. Rscript ~/scripts/maplot.r publdsur.shp
-# e.g. Rscript ~/scripts/maplot.r "~/Documents/GEE/python/publdsur.shp"
+# README:
+# A Rscript file to plot a single shapefile from Linux terminal for data checking
+# sometimes I have an urge to see my shapefile with coordinates quickly, without opening QGIS -
+# e.g. Rscript maplot.r "~/Documents/GEE/python/publdsur.shp"
 
 # required (needs to be installed prior in R) for maptools = sp, rgeos
-library(maptools)
-input <-commandArgs(TRUE)
 # normally, this commandArgs() - without (TRUE) - come with the following arguments
+# print(commandArgs)
 # [1] "/usr/lib/R/bin/exec/R"               
 # [2] "--slave"                             
 # [3] "--no-restore"                        
@@ -14,12 +15,14 @@ input <-commandArgs(TRUE)
 # [5] "--args"                              
 # [6] "publdsur.shp" 
 
-print(input)
+library(maptools)
+
+input <-commandArgs(TRUE)
 
 shp <- readShapeSpatial(input[1])
-# print(class(shp))
 
 X11()
+
 plot(shp, border = "blue", pch = 10, axes = T)
 
 # pause to give time to examine the plot
