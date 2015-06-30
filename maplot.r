ff76380b96d5419e6b26ee6c825f9c44e983ba14
cmaene <cmaene@gmail.com>
2015-06-30 16:34:00 -0500

    switch to rgdal

--- maplot.r
+++ maplot.r
@@ -3,7 +3,8 @@
 # README:
 # This is an Rscript to plot a single shapefile from Linux terminal for data checking -
 # i.e. sometimes I have an urge to see shapefile visually (and witih coordinates!) without opening QGIS -
-# e.g. Rscript maplot.r "~/GEE/python/publdsur.shp"
+# e.g. ./scripts/maplot.r "~/GEE/python/publdsur.shp"
+# (or loudly..) Rscript ~/scripts/maplot.r "publdsur.shp"
 
 # required (needs to be installed prior in R) for maptools = sp, rgeos
 # normally, this commandArgs() - without (TRUE) - come with the following arguments
@@ -15,11 +16,24 @@
 # [5] "--args"                              
 # [6] "publdsur.shp" 
 
-library(maptools)
+library(rgdal)
 
 input <-commandArgs(TRUE)
 
-shp <- readShapeSpatial(input[1])
+# readOGR is a bit goofy - I need to give a data source (directory)
+dsn   <-dirname(input[1])
+if(is.null(dsn)){
+  dsn <-"."
+}
+
+# AND also file name without extension
+layer=sub("^([^.]*).*", "\\1", basename(input[1]))
+
+# print the layer information
+ogrInfo(dsn=dsn, layer=layer)
+
+# plot the layer on a new window..
+shp <- readOGR(dsn=dsn, layer=layer, verbose=F)
 
 X11()
 
